687aa51c395f49e110bf68d78a5e5515656abf39
JavGog <javierggjavier@gmail.com>
2022-09-14 13:04:36 -0700

    new Ogre class and new Dragon class added to magicians folder.

---
+++ src/main/java/players/magicians/Ogre.java
@@ -0,0 +1,13 @@
+package players.magicians;
+
+public class Ogre {
+    private String punch = "Punch";
+
+    public String getPunch() {
+        return punch;
+    }
+
+    public Ogre(String punch) {
+        this.punch = punch;
+    }
+}
--- src/main/java/players/magicians/WeaponMagicians.java
+++ src/main/java/players/magicians/WeaponMagicians.java
@@ -1,4 +1,8 @@
 package players.magicians;
 
 public interface WeaponMagicians {
+    Dragon dragon = null;
+    Ogre ogre = null;
+    void fireBall();
+    void lightningStrike();
 }
